e529d841da7024c4d8441103a38297ecf2fdc86d
Justinas Delinda <8914032+minht11@users.noreply.github.com>
2024-12-09 00:01:44 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div></div>
+
+<style>
+	@import '../app.css' reference;
+	/* @media (width >= theme(--breakpoint-sm))  */
+	@media (min-width: 600px) {
+		div {
+			background-color: red;
+		}
+	}
+</style>
